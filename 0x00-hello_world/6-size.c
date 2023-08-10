1159c185bb28104cf1a400faa0d0fb32c61a20bb
kenny <kennynet66@gmail.com>
2023-08-10 15:44:19 -0700

    New change

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-	/* Declaration section */
 	int c = sizeof(char);
 	int i = sizeof(int);
 	int li = sizeof(long int);
 	int ll = sizeof(long long int);
 	int f = sizeof(float);
+
 	printf("Size of a char: %d byte(s)\n", c);
 	printf("Size of an int: %d byte(s)\n", i);
 	printf("Size of a long int: %d byte(s)\n", li);
