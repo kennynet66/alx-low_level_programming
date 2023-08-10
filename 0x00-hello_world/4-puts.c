41df4c4f95d7d9be7c9ad4dcae4f96b1ffed99a5
kenny <kennynet66@gmail.com>
2023-08-10 15:02:04 -0700

    C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/*
+ * This is the main file
+ * It will return 0
+ */
 int main(void)
 {
-	puts(" \"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
