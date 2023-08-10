b10d84720cb4afff8c86fa410aa1758fb048b184
kenny <kennynet66@gmail.com>
2023-08-10 15:37:36 -0700

    New change

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
- /**
-  * main - This is where the program starts running
-  * print the size of various types
-  *
-  * Return: Always 0 (Success)
-  */
- int main(void)
- {
+/**
+ * main - This is where the program starts running
+ * print the size of various types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	/* Declaration section */
 	int c = sizeof(char);
 	int i = sizeof(int);
