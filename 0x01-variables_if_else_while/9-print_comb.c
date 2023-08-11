d88c4ba7f13aac45d3e2e5cbb7b5d341af60bd7f
kenny <kennynet66@gmail.com>
2023-08-11 15:49:10 -0700

    New numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,18 +6,15 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 10; i++)
 	{
-		for (j = i; j <= 9; j++)
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 9 || j != 9)
-			{
-				putchar(',');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
