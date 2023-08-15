d42b52f00ff52f43d5cf09112696e0e95bd82da7
kenny <kennynet66@gmail.com>
2023-08-15 10:14:48 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the program entry point
+ * the program prints _putchar
+ * Return: 0 success exit status code
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
