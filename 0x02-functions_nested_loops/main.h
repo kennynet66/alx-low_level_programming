f6998392d61d379553d49d26c08707f8556f4af5
kenny <kennynet66@gmail.com>
2023-08-19 05:33:38 -0700

    New change

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -16,9 +16,8 @@ void _putchar(char c)
 void print_alphabet_x10(void)
 {
 	char letter;
-
 	for (int num = 1; num <=10; num++)
-		for (letter = 'a'; letter <= 'z'; letter++)
+		for(char letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
