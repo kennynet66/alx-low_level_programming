f93ca03fcb6a68a08a83e743411437e92baeef4f
kenny <kennynet66@gmail.com>
2023-08-19 05:28:22 -0700

    10 alphabets

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,16 @@ void _putchar(char c)
 	write(1, &c, 1);
 }
 
+void print_alphabet_x10(void)
+{
+	char letter;
+
+	for (int num = 1; num <=10; num++)
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+	letter = 'a';
+	_putchar('\n');
+}
 #endif
