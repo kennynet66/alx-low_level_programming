7c9fd102a8b1bc6c4e19d0d47387dd2cf245ee17
kenny <kennynet66@gmail.com>
2023-08-19 06:03:35 -0700

    _putchar

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-This is a non-empty readme
+0x02-functions_nested_loops
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,29 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
-#include <stdio.h>
-#include <unistd.h>
-/**
-* main - This is the program entry point
-* the program prints _putchar
-* Return: 0 success exit status code
-*/
-void _putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void print_alphabet_x10(void)
-{
-	char letter;
-	int num;
-
-	for (num = 1; num <=10; num++)
-		for(letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
-	letter = 'a';
-	_putchar('\n');
-}
-#endif
+int _putchar(char);
