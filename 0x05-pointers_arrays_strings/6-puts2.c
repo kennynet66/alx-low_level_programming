dac7ed07bfa883af8a413ebb45c9536d233ac400
kenny <kennynet66@gmail.com>
2023-08-21 16:58:03 -0700

    New change

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <main.h>
+/**
+ * prints - all chars of a string on a new line
+ * @str - string to be modified
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
