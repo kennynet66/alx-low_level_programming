093942baa952aac399b358960fd4555b8829509f
kenny <kennynet66@gmail.com>
2023-08-21 16:04:39 -0700

    New change

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - Reverse string provided
+ * @s: Input pointe
+ */
+void rev_string(char *s)
+{
+	int lengtj = strlen(s);
+	int i;
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n);
+}
