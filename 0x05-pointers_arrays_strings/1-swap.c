4cecedfa3a78559e2f36598a9a420d343a2629c1
kenny <kennynet66@gmail.com>
2023-08-21 13:55:20 -0700

    New change

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 /**
  * swap_int - swaps the values of the ints
+ * @a: Int input pointer
+ * @b: Int input pointer
  * Return: nothing
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int temp = *a;
+	*a = *b;
+	*b = *a;
 }
