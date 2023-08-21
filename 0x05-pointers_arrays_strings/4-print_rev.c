35366bbd7776552b4bedaff6cb03e20b75186eb5
kenny <kennynet66@gmail.com>
2023-08-21 15:35:47 -0700

    New change

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,10 @@
 void print_rev(char *s)
 {
 	int length = strlen(s);
-
-	for (int i = length - 1; i >= 0; i--;)
+	int i = length - 1;
+	for (i; i >= 0; i--)
 	{
 		putchar(s[i]);
 	}
-	putchar('\n')
+	putchar('\n');
 }
