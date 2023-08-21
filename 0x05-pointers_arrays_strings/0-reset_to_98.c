3c2d07a074068bbea4dd433d98bd15d1ee022bd1
kenny <kennynet66@gmail.com>
2023-08-21 13:09:13 -0700

    New change

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,20 @@
 #include "main.h"
-/* main - Programs Entry point
+/**
+ * main - changes the value of n to 98 using pointers
+ * reset_to_98 - Resets the value it points at to 98
  * Return: Always 0
  */
 
 int main(void)
+{
+	int n = 402;
+
+	printf("Before the call n is %d\n", n);
+	reset_to_98(&n);
+	printf("n =%d", n);
+}
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
