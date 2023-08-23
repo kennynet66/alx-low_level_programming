ca5033c4f85c981fbff7d44f798ebe7610e6dfed
kenny <kennynet66@gmail.com>
2023-08-23 15:18:07 -0700

    New change

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,26 +13,12 @@ char *_strcat(char *dest, char *src)
 {
 	int len1 = strlen(dest);
 	int len2 = strlen(src);
+	int i;
 
-	for (int i = 0; i <= len2; i++)
+	for (i = 0; i <= len2; i++)
 	{
 		dest[len1 + i] = src[i];
 	}
 	dest[len1 + len2] = '\0';
 	return (dest);
-}
-
-int main(void)
-{
-    char s1[30] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
-
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strcat(s1, s2);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
 }
