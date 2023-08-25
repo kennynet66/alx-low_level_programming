357db62ab23266b9d9ede5f914c6193c8d5da98f
kenny <kennynet66@gmail.com>
2023-08-24 18:42:19 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,11 +12,11 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-	if (s1[i] != s2[i])
-	{
-		flag = s1[i] - s2[i];
-		break;
+		if (s1[i] != s2[i])
+		{
+			flag = s1[i] - s2[i];
+			break;
+		}
 	}
-		return (flag);
-	}
+	return (flag);
 }
